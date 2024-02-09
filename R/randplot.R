1c71aaf8962dae9647f8284bc31f94ccce176707
Jonas Metzger <Jonas.A.Metzger-1@ou.edu>
2024-02-09 13:41:48 -0600

    added myplot function

---
+++ R/randplot.R
@@ -0,0 +1,12 @@
+#' Random Plot function
+#'
+#' @param x numeric vector
+#'
+#' @return numeric vector
+#' @export
+#'
+#' @examples
+#' myplot(x = 1:100)
+myplot=function(x){
+  0.86089580 +1.46959217*x  -0.02745726*x^2
+}
